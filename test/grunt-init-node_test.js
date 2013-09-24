eb08f861e6ea40be59e53638fe07e29ee466a0f1
Todd Wolfson <todd@twolfson.com>
2013-09-23 22:03:37 -0700

    Added test prompt feedback for UNLICENSE init

--- test/grunt-init-node_test.js
+++ test/grunt-init-node_test.js
@@ -7,7 +7,7 @@ var assert = require('assert'),
 describe('An UNLICENSE init', function () {
   before(function (done) {
     // Relocate to the test directory
-    var testDir = __dirname + '/test/actual_files/unlicense';
+    var testDir = __dirname + '/actual_files/unlicense';
     rimraf.sync(testDir);
     mkdirp.sync(testDir);
     process.chdir(testDir);
@@ -15,6 +15,22 @@ describe('An UNLICENSE init', function () {
     // Run the grunt-init script inside of the test directory
     suppose('grunt-init', ['node'])
       .debug(process.stdout)
+      .on(/Project name/).respond('test-unlicense\n')
+      .on(/Description/).respond('Unlicense test project\n')
+      .on(/Version/).respond('9.0.0\n')
+      .on(/Project git repository/).respond('git://abc\n')
+      .on(/Project homepage/).respond('http://abc\n')
+      .on(/Project issues tracker/).respond('http://abc/issues\n')
+      .on(/Licenses/).respond('Unlicense\n')
+      .on(/Author name/).respond('Todd Wolfson\n')
+      .on(/Author email/).respond('todd@twolfson.com\n')
+      .on(/Author url/).respond('http://twolfson.com/\n')
+      .on(/Gittip username/).respond('\n')
+      .on(/What versions of node/).respond('>= 0.8.0\n')
+      .on(/Main module\/entry point/).respond('lib/test-unlicense\n')
+      .on(/Npm test command/).respond('mocha\n')
+      .on(/What keywords/).respond('a, b, c\n')
+      .on(/any changes/).respond('n\n')
       ['error'](done)
       .end(function (code) {
         assert.strictEqual(code, 0, 'Exited with non-zero code (' + code + ')');
