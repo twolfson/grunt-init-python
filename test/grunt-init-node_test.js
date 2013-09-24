03ca460b3fe4eef30c7f7487de0038174349334a
Todd Wolfson <todd@twolfson.com>
2013-09-23 21:57:18 -0700

    Adding clean up to test script

--- test/grunt-init-node_test.js
+++ test/grunt-init-node_test.js
@@ -1,14 +1,24 @@
 // Load in depdendencies
-var suppose = require('suppose'),
-    assert = require('assert');
+var assert = require('assert'),
+    suppose = require('suppose'),
+    rimraf = require('rimraf'),
+    mkdirp = require('mkdirp');
 
 describe('An UNLICENSE init', function () {
   before(function (done) {
+    // Relocate to the test directory
+    var testDir = __dirname + '/test/actual_files/unlicense';
+    rimraf.sync(testDir);
+    mkdirp.sync(testDir);
+    process.chdir(testDir);
+
+    // Run the grunt-init script inside of the test directory
     suppose('grunt-init', ['node'])
       .debug(process.stdout)
       ['error'](done)
       .end(function (code) {
-        console.log(code);
+        assert.strictEqual(code, 0, 'Exited with non-zero code (' + code + ')');
+        done();
       });
   });
 
