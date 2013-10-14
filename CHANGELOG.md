270c3c06019842bc559c64854feeffbec67e18c0
Todd Wolfson <todd@twolfson.com>
2013-10-14 00:41:37 -0700

    Continuing boilerplate files

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,18 +1,2 @@
 # grunt-init-node changelog
-0.4.1 - Fixed failing tests in Travis CI
-
-0.4.0 - Added tests, Travis CI, and fixed UNLICENSE not showing in package.json
-
-0.3.1 - Corrected trailing README whitespace
-
-0.3.0 - Added Unlicense and set it as default
-
-0.2.3 - Use bleeding edge gittip-badge
-
-0.2.2 - More tweaks to phrasing
-
-0.2.1 - Corrected phrasing for Gittip badge
-
-0.2.0 - Updated Gittip badge to larger format
-
-0.1.0 - Initial fork from gruntjs/grunt-init-node
+0.1.0 - Initial fork from twolfson/grunt-init-node
---
+++ test/expected_files/unlicense/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='sublime_plugin_tests',
+    version='0.4.0',
+    description='Testing framework for Sublime Text plugins',
+    long_description=open('README.rst').read(),
+    keywords=[
+        'sublime text',
+        'plugin',
+        'test',
+        'framework',
+        'tdd'
+    ],
+    author='Todd Wolfson',
+    author_email='todd@twolfson.com',
+    url='https://github.com/twolfson/sublime-plugin-tests',
+    download_url='https://github.com/twolfson/sublime-plugin-tests/archive/master.zip',
+    packages=find_packages(),
+    license='UNLICENSE',
+    install_requires=open('requirements.txt').readlines(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Text Editors'
+    ]
+)
